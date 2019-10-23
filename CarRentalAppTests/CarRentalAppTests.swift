2319fd4da9b513cd1ed8067309cc738279531cfb
AndriyZaec <andrew.zaec@gmail.com>
2019-10-23 18:44:14 +0300

    Initial Commit

---
+++ CarRentalAppTests/CarRentalAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CarRentalAppTests.swift
+//  CarRentalAppTests
+//
+//  Created by Andrew on 23.10.2019.
+//  Copyright © 2019 Andrew Zaiets. All rights reserved.
+//
+
+import XCTest
+@testable import CarRentalApp
+
+class CarRentalAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
